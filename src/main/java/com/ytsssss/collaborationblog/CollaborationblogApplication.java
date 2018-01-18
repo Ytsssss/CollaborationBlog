18c65af2ff0cd530b8c93bc708d07cd1d9ca4811
yegk7 <yegk7@ytsssss.ufsoft.com.cn>
2018-01-18 12:27:57 +0800

    creat project

---
+++ src/main/java/com/ytsssss/collaborationblog/CollaborationblogApplication.java
@@ -0,0 +1,12 @@
+package com.ytsssss.collaborationblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborationblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborationblogApplication.class, args);
+	}
+}
