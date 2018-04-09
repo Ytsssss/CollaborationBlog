48d04bcc69b970f91752d1176a851fd9953fe428
ytsssss <995170811@qq.com>
2018-04-09 10:19:53 +0800

    提交代码

---
+++ src/main/java/com/ytsssss/collaborationblog/vo/BlogDetailVO.java
@@ -0,0 +1,85 @@
+package com.ytsssss.collaborationblog.vo;
+
+import java.util.Date;
+
+public class BlogDetailVO {
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String createTime;
+
+    private Long readTime;
+
+    private String userAvatar;
+
+    private String userName;
+
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getReadTime() {
+        return readTime;
+    }
+
+    public void setReadTime(Long readTime) {
+        this.readTime = readTime;
+    }
+
+    public String getUserAvatar() {
+        return userAvatar;
+    }
+
+    public void setUserAvatar(String userAvatar) {
+        this.userAvatar = userAvatar;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
